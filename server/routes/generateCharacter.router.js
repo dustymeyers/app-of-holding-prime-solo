1a23200c1173e677c54a465504a9991dd8d06150
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-03-18 10:07:03 -0500

    rewrote sql queries already in place, simplifying

--- server/routes/generateCharacter.router.js
+++ server/routes/generateCharacter.router.js
@@ -14,9 +14,19 @@ router.get('/:playStyle/:magicStyle', rejectUnauthenticated, (req, res) => {
   const playStyleParameters = req.params.playStyle;
   const magicStyleParameters = req.params.magicStyle;
 
+  // The ids of the classes range from 1-9, this will "randomize" a number within that range
+  const randomRaceId = Math.floor(Math.random() * 9) + 1;
+
   /**
    * This is what our Class Parameter query will look like:
    * 
+   * SELECT "classes".id FROM "classes"
+   * WHERE "play_style" = 'hackAndSlash' AND "magic_style" = 'noMagic';
+   * 
+   * 
+   * 
+   * 
+   *  DEPRECATED FOR NOW
    * SELECT 
    *    "classes".id,
    *    "classes".class_name
@@ -29,28 +39,77 @@ router.get('/:playStyle/:magicStyle', rejectUnauthenticated, (req, res) => {
    * GROUP BY "classes".id; 
    */
 
-  const classParameterQuery = `
-    SELECT 
-      "classes".id,
-      "classes".class_name
+  // this will get us back an id number that we can randomize within parameters of user input
+  const classIdParameterQuery = `
+    SELECT "classes".id FROM "classes"
+    WHERE "play_style" = $1 AND "magic_style" = $2;
+  `;
+
+  // this will return a random class depending on tha value of randomRaceId
+  const raceInfoQuery = `
+    SELECT * FROM "races" WHERE "id" = $1;
+  `;
+
+  // this will return all class and class feature information to the chosen class
+  const classInfoQuery = `
+    SELECT "classes".*, 
+      ARRAY_AGG(ARRAY["features".feature_name, "features".feature_description]) AS class_features
     FROM "classes"
     JOIN "classes_features"
       ON "classes".id = "classes_features".class_id
     JOIN "features"
       ON "classes_features".feature_id = "features".id
-    WHERE "play_style" = $1 AND "magic_style" = $2
-    GROUP BY "classes".id;`;
+    WHERE "classes".id = $1
+    GROUP BY "classes".id;
+  `;
+
+  // this will return all of the race features for the chosen race,
+  // had to separate from original race query
+  const raceFeatures = `
+    
+  `; 
 
   // first query to get back classes that fit the description
   pool
-    .query(classParameterQuery, [ playStyleParameters, magicStyleParameters ])
-    .then(dbRes => {
-      console.log(`GET /api/generateCharacter/${playStyleParameters}/${magicStyleParameters} RESPONSE:`, dbRes.rows);
+    .query(classIdParameterQuery, [ playStyleParameters, magicStyleParameters ])
+    .then(classIdResponse => {
+      console.log(
+        `GET /api/generateCharacter/${playStyleParameters}/${magicStyleParameters} RESPONSE:`, 
+        classIdResponse.rows
+      );
 
+      // Randomizes the class that we get back within the parameters
+      const randomClassId = classIdResponse.rows[
+        Math.floor(Math.random() * classIdResponse.rows.length)
+      ].id;
+      
+      pool // second query to get back random race info
+        .query(raceInfoQuery, [randomRaceId])
+        .then(raceResponse => {
+          console.log('Fetching race data related to id:', randomRaceId);          
 
+          pool // third query to get back class information now that class has been chosen
+            .query(classInfoQuery, [randomClassId])
+            .then(classInfoResponse => {
+              console.log('Fetching class data related to id:', randomClassId);
 
-      res.send(dbRes.rows[Math.floor(Math.random() * dbRes.rows.length)]);
+              res.send({classInfo: classInfoResponse.rows, raceInfo: raceResponse.rows});
+            })
+            // third catch
+            .catch(classError => {
+              console.log(`Error using class id ${randomClassId} to get class info`, classError);
+
+              res.sendStatus(500);
+            })
+          })
+        // second catch for race
+        .catch(raceError => {
+          console.log(`Error using race id ${randomRaceId} to get race info`, raceError);
+
+          res.sendStatus(500);
+        })      
     })
+    // first catch 
     .catch(err => {
       console.error(`GET /api/generateCharacter/${playStyleParameters}/${magicStyleParameters} ERROR:`, err);
       res.sendStatus(500);
