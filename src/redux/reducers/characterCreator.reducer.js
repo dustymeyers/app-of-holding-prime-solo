bc9abaf088d7e146ee52958a588852f862a0d97e
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-03-18 11:59:56 -0500

    sagas send data to reducer!

---
+++ src/redux/reducers/characterCreator.reducer.js
@@ -0,0 +1,10 @@
+const characterCreatorReducer = (state = {}, action) => {
+  switch (action.type){
+    case 'SET_GENERATED_CHARACTER':
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default characterCreatorReducer;
--- src/redux/sagas/characterCreator.saga.js
+++ src/redux/sagas/characterCreator.saga.js
@@ -1,2 +1,20 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
+
+function* generateRandomCharacter(action) {
+  try {
+    // get class info pertaining to user parameters for user review
+    const randomCharacter = yield axios.get(`/api/generateCharacter/${action.payload.playStyle}/${action.payload.magicStyle}`);
+
+    // save it in redux state for user to review
+    yield put({ type: 'SET_GENERATED_CHARACTER', payload: randomCharacter.data})
+  } catch (error) {
+    console.log('Error with character generator:', error);
+  }
+} // en generateRandomCharacter
+
+function* characterCreationSaga() {
+  yield takeLatest('GET_RANDOM_CHARACTER', generateRandomCharacter);
+} // end characterCreationSaga
+
+export default characterCreationSaga;
